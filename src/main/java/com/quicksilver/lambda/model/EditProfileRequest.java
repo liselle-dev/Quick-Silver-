028de963cbc4c10e496cf83327765f7ab38728c4
Rubinapatel-alt <Rubinapatel65@yahoo.com>
2026-01-07 09:21:58 -0500

    Create EditProfileRequest.java

---
+++ src/main/java/com/quicksilver/lambda/model/EditProfileRequest.java
@@ -0,0 +1,34 @@
+package com.quicksilver.lambda.model;
+
+public class EditProfileRequest {
+
+    private String userId;
+    private String name;
+    private String email;
+
+    public EditProfileRequest() {}
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
